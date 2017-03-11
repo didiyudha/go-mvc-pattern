bc7af0ccba99324a6de42600b4ec34e8cbe69061
didiyudha <didiyudha@gmail.com>
2017-03-11 16:07:30 +0700

    Refactoring parsing function so that all the gohtml file can be read in views directoru

--- templates/templates.go
+++ templates/templates.go
@@ -1,8 +1,30 @@
 package templates
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
-// Template define and take directory where the templates are stored
+// Template parse template from views directory
 func Template() *template.Template {
-	return template.Must(template.ParseGlob("views/*"))
+	templ := template.New("")
+	err := filepath.Walk("./views", func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, ".gohtml") {
+			_, err = templ.ParseFiles(path)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
+		return err
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return templ
 }
